799393b42fa2f7e2566297cdf0d6bc13dfff4b35
csaflip <colin.adams@ufl.edu>
2019-11-13 10:09:39 -0500

    Added version number, timestamp to block.

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,13 @@
 import hashlib
+import datetime
 
 
 class Block:
-    def __init__(self, previous_hash, transactions: str):
+    def __init__(self, previous_hash, transactions: str, version: float):
         self.previousHash = previous_hash
         self.transactions = transactions
+        self.stamp = datetime.time()
+        self.version = version
 
     def get_blockhash(self):
         blockhash = hashlib.sha256()
@@ -17,3 +20,9 @@ class Block:
 
     def get_previous_block_hash(self):
         return self.previousHash
+
+    def get_timestamp(self):
+        return self.stamp
+
+    def get_version(self):
+        return self.version
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from blockchain import Block
 
 genesis_transaction = 'This is the first transaction'
 
-genesisBlock = Block(0, genesis_transaction)
+genesisBlock = Block(0, genesis_transaction, 1)
 
 print(genesisBlock.get_blockhash())
 
-second = Block(genesisBlock.get_blockhash(), 'This is the second transaction')
+second = Block(genesisBlock.get_blockhash(), 'This is the second transaction', 1)
 
 print(second.get_blockhash())
 
-third = Block(second.get_blockhash(), 'This is my third message.')
+third = Block(second.get_blockhash(), 'This is my third message.', 1)
 
 print(third.get_blockhash())
 
