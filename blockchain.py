fae1083f1fdc39b1586e47fc9c54882d38cbe2e5
csaflip <colin.adams@ufl.edu>
2019-12-03 16:42:44 -0500

    Added Transaction and file support to the chain. Set up an example block chain.

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,38 @@
 import hashlib
 import datetime
+from typing import List, Set, Dict, Tuple, Optional
+
+
+
+class Transaction:
+    def __init__(self, filename: str):
+        self.hash = ''
+        self.filename = filename
+        with open(filename, 'rb') as f:
+            h1 = hashlib.sha256(f.read()).hexdigest()  # hashes contents of file
+            self.hash = h1
+
+    def get_hash(self):
+        if self.hash == '':
+            return self.filename
+        else:
+            return self.hash
 
 
 class Block:
-    def __init__(self, previous_hash, transactions: str, version: float):
+    def __init__(self, previous_hash, transactions: List[Transaction], version: int):
         self.previousHash = previous_hash
         self.transactions = transactions
         self.stamp = datetime.time()
         self.version = version
 
     def get_blockhash(self):
+        tophash = ''
+        for transaction in self.transactions:
+            tophash += transaction.get_hash()
         blockhash = hashlib.sha256()
         blockhash.update(str(self.previousHash).encode())
-        blockhash.update(self.transactions.encode())
+        blockhash.update(tophash.encode())
         return blockhash.hexdigest()
 
     def get_transactions(self):
@@ -26,3 +46,4 @@ class Block:
 
     def get_version(self):
         return self.version
+
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from blockchain import Block
+from blockchain import Block, Transaction
 
-genesis_transaction = 'This is the first transaction'
 
+
+
+
+
+##SAMPLE BLOCKCHAIN EXAMPLE
+
+#Genesis block building
+genesis_transaction = []
+genesis_transaction.append(Transaction('sample.pdf'))
 genesisBlock = Block(0, genesis_transaction, 1)
 
+##Adding blocks with different transcations
+second_transactions = []
+second_transactions.append(Transaction('test.pdf'))
+second_transactions.append(Transaction('docfile.docx'))
+second = Block(genesisBlock.get_blockhash(), second_transactions, 1)
+
+third_transactions = []
+third_transactions.append(Transaction('R1.docx'))
+third_transactions.append(Transaction('main.py'))
+third = Block(second.get_blockhash(), third_transactions, 1)
+
+
 print(genesisBlock.get_blockhash())
-
-second = Block(genesisBlock.get_blockhash(), 'This is the second transaction', 1)
-
 print(second.get_blockhash())
-
-third = Block(second.get_blockhash(), 'This is my third message.', 1)
-
 print(third.get_blockhash())
+
+
+
